e41d6f19c2d6d0a71e04a2d0e4b38f2cb72a0dac
harlecin <christoph.bodner@gmail.com>
2019-04-01 14:31:44 +0200

    azr_build_image function skeleton

---
+++ serverless/R/azr_build_image.R
@@ -0,0 +1,10 @@
+#' Upload and build a Container using Azure Container Registry (ACR)
+#'
+#' Upload local files to Azure Container Registry and build a Docker image
+#' as specified in the Dockerfile
+#'
+#' @param path Path to the folder with source code and Dockerfile to use
+#' @return Status code
+azr_build_image = function(path = ".") {
+
+}
